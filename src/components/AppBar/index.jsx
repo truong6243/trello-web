9e21f75d8303e06e9b2cf48d0b4af977c1a6471f
truongdev1503 <127965911+truongdev1503@users.noreply.github.com>
2026-03-21 00:40:55 +0700

    app_bar

--- src/components/AppBar/index.jsx
+++ src/components/AppBar/index.jsx
@@ -1,15 +1,25 @@
-import  Box  from "@mui/material/Box"
+import Box from "@mui/material/Box"
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import AppsIcon from '@mui/icons-material/Apps';
+import { SvgIcon } from "@mui/material";
+ import TrelloIcon from '~/assets/trello.svg?react'
 const AppBar = () => {
   return (
     <>
-      <Box sx={{
+      <Box px = {2} sx={{
         backgroundColor: 'primary.light',
         height: (theme) => theme.trello.headerHeight,
         width: '100%',
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        
       }}>
-        App Bar
+        <Box >
+          <AppsIcon sx={{ color: 'primary.main'}} />
+          <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color: 'primary.main'}} />
+        </Box>
+        <Brightness4Icon />
       </Box>
     </>
   )
--- src/theme.js
+++ src/theme.js
@@ -2,20 +2,32 @@ import { extendTheme } from "@mui/material";
 
 const theme = extendTheme({
   trello: {
-    headerHeight: '48px',
-    boardBarHeight: '58px'
+    headerHeight: "48px",
+    boardBarHeight: "58px",
   },
   colorSchemes: {
     light: {
-      colors: {
-        background: "#f9f9f9",
-        foreground: "#121212",
+      palette: {
+        // Đổi 'colors' thành 'palette'
+        background: {
+          default: "#f9f9f9", // MUI sẽ tự động lấy màu này làm nền trang web
+          paper: "#ffffff", // Nền cho các component nổi lên như thẻ Card, Modal
+        },
+        text: {
+          primary: "#121212", // MUI tự động lấy màu này làm màu chữ chính
+        },
       },
     },
     dark: {
-      colors: {
-        background: "#212121",
-        foreground: "#fff",
+      palette: {
+        // Đổi 'colors' thành 'palette'
+        background: {
+          default: "#212121",
+          paper: "#2c2c2c",
+        },
+        text: {
+          primary: "#fff",
+        },
       },
     },
   },
