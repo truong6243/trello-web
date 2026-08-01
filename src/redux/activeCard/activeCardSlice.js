151103ba7da1707d4ca3b0c4a9009c9f72c47902
truongdev1503 <127965911+truongdev1503@users.noreply.github.com>
2026-08-01 09:59:54 +0700

    fix_open_modal_card

--- src/redux/activeCard/activeCardSlice.js
+++ src/redux/activeCard/activeCardSlice.js
@@ -3,7 +3,8 @@
 import { createSlice } from '@reduxjs/toolkit'
 // khởi tạo giá trị của một slice trong redux
 const initialState = {
-  currentActiveCard: null
+  currentActiveCard: null,
+  isShowModalActiveCard: false
 }
 
 // khởi tạo một slice trong kho lưu trữ redux store
@@ -12,8 +13,12 @@ export const activeCardSlice = createSlice({
   initialState,
   // nơi xử lý dữ liệu đồng bộ
   reducers: {
-    clearCurrentActiveCard: (state) => {
+    showModalActiveCard: (state) => {
+      state.isShowModalActiveCard = true
+    },
+    clearAndHideCurrentActiveCard: (state) => {
       state.currentActiveCard = null
+      state.isShowModalActiveCard = false
     },
     updateCurrentActiveCard: (state, action) => {
       const fullCard = action.payload
@@ -30,13 +35,19 @@ export const activeCardSlice = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { clearCurrentActiveCard, updateCurrentActiveCard } = activeCardSlice.actions
+export const {
+  clearAndHideCurrentActiveCard,
+  updateCurrentActiveCard,
+  showModalActiveCard
+} = activeCardSlice.actions
 
 // Selectors: là nơi dành cho các components bên dưới goi bằng hook useSelector()
 // để lấy  dữ liệu từ trong kho redux store ra sử dụng
 export const selectorCurrentActiveCard = (state) => {
   return state.activeCard.currentActiveCard
 }
+export const selectorIsShowModalActiveCard = (state) => {
+  return state.activeCard.isShowModalActiveCard
+}
 
 export default activeCardSlice.reducer
-  
